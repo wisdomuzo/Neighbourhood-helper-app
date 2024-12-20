ec6bb22bff70aae22023726ec218fa2275ce63be
Uzo Wisdom <wisdomuzo123@gmail.com>
2024-12-20 16:44:39 +0100

    sixth commit

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
-set -0 errexit
-pip install -r requirements.txt
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies efficiently
+python -m pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
+
+# Collect static files and migrate
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py runserver
